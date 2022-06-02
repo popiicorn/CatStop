790de3fd2af5f9c4bb6b7d0767faf5d97d37b0e7
ポッピコーン <43871609+popiicorn@users.noreply.github.com>
2022-06-02 21:18:42 +0900

    着せ替え

--- Assets/Scripts/EquipmentPoint.cs
+++ Assets/Scripts/EquipmentPoint.cs
@@ -7,10 +7,11 @@ public class EquipmentPoint : MonoBehaviour
 {
     int point = 0;
     public Text textEquipment;
+    public GameObject[] equipmentImage;
 
 
 
-    // Start is called before the first frame update
+
     void Start()
     {
         //ポイント値のロード
@@ -20,9 +21,55 @@ public class EquipmentPoint : MonoBehaviour
         textEquipment.text = "" + point;
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
+        if (point == 1)
+        {
+            equipmentImage[1].SetActive(true);
+        }
+
+        if (point == 2)
+        {
+            equipmentImage[2].SetActive(true);
+            Debug.Log("装備");
+        }
+
+        if (point == 3)
+        {
+            equipmentImage[3].SetActive(true);
+        }
+
+        if (point == 4)
+        {
+            equipmentImage[4].SetActive(true);
+        }
+
+        if (point == 5)
+        {
+            equipmentImage[5].SetActive(true);
+        }
+
+        if (point == 6)
+        {
+            equipmentImage[6].SetActive(true);
+        }
+
+        if (point == 7)
+        {
+            equipmentImage[7].SetActive(true);
+        }
+
+        if (point == 8)
+        {
+            equipmentImage[8].SetActive(true);
+        }
+
+        if (point == 9)
+        {
+            equipmentImage[9].SetActive(true);
+        }
 
     }
 
@@ -36,60 +83,80 @@ public class EquipmentPoint : MonoBehaviour
         ES3.Save<int>("Pointkey", point);
     }
 
+    //装備前消す------------------------------------------
+    void AllFalse()
+    {
+        for (int i = 0; i < equipmentImage.Length; i++)
+        {
+            equipmentImage[i].gameObject.SetActive(false);
+        }
+    }
+
+
+
     //オンクリック--------------------------------------------
 
     public void OnClickPoint_1()
     {
         point = 1;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_2()
     {
         point = 2;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_3()
     {
         point = 3;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_4()
     {
         point = 4;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_5()
     {
         point = 5;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_6()
     {
         point = 6;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_7()
     {
         point = 7;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_8()
     {
         point = 8;
         PointLabel();
+        AllFalse();
     }
 
     public void OnClickPoint_9()
     {
         point = 9;
         PointLabel();
+        AllFalse();
     }
 
 
