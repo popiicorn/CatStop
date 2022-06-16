388bad8d1d653bf8454446aae2344a18ee5aee20
ポッピコーン <43871609+popiicorn@users.noreply.github.com>
2022-06-16 21:08:00 +0900

    インゲームでの衣装替え＆ボタン選択実装

--- Assets/Scripts/ButtonSelect.cs
+++ Assets/Scripts/ButtonSelect.cs
@@ -61,6 +61,185 @@ public class ButtonSelect : MonoBehaviour
             select[9].SetActive(true);
         }
 
+        if (equipmentpoint.point == 10)
+        {
+            select[10].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 11)
+        {
+            select[11].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 12)
+        {
+            select[12].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 13)
+        {
+            select[13].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 14)
+        {
+            select[14].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 15)
+        {
+            select[15].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 16)
+        {
+            select[16].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 17)
+        {
+            select[17].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 18)
+        {
+            select[18].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 19)
+        {
+            select[19].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 20)
+        {
+            select[20].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 21)
+        {
+            select[21].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 22)
+        {
+            select[22].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 23)
+        {
+            select[23].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 24)
+        {
+            select[24].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 25)
+        {
+            select[25].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 26)
+        {
+            select[26].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 27)
+        {
+            select[27].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 28)
+        {
+            select[28].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 29)
+        {
+            select[29].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 30)
+        {
+            select[30].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 31)
+        {
+            select[31].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 32)
+        {
+            select[32].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 33)
+        {
+            select[33].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 34)
+        {
+            select[34].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 35)
+        {
+            select[35].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 36)
+        {
+            select[36].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 37)
+        {
+            select[37].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 38)
+        {
+            select[38].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 39)
+        {
+            select[39].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 40)
+        {
+            select[40].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 41)
+        {
+            select[41].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 42)
+        {
+            select[42].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 43)
+        {
+            select[43].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 44)
+        {
+            select[44].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 45)
+        {
+            select[45].SetActive(true);
+        }
     }
 
 
--- Assets/Scripts/EquipmentInGame.cs
+++ Assets/Scripts/EquipmentInGame.cs
@@ -62,6 +62,185 @@ public class EquipmentInGame : MonoBehaviour
             equipmentInGame[9].SetActive(true);
         }
 
+        if (equipmentpoint.point == 10)
+        {
+            equipmentInGame[10].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 11)
+        {
+            equipmentInGame[11].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 12)
+        {
+            equipmentInGame[12].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 13)
+        {
+            equipmentInGame[13].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 14)
+        {
+            equipmentInGame[14].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 15)
+        {
+            equipmentInGame[15].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 16)
+        {
+            equipmentInGame[16].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 17)
+        {
+            equipmentInGame[17].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 18)
+        {
+            equipmentInGame[18].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 19)
+        {
+            equipmentInGame[19].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 20)
+        {
+            equipmentInGame[20].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 21)
+        {
+            equipmentInGame[21].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 22)
+        {
+            equipmentInGame[22].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 23)
+        {
+            equipmentInGame[23].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 24)
+        {
+            equipmentInGame[24].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 25)
+        {
+            equipmentInGame[25].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 26)
+        {
+            equipmentInGame[26].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 27)
+        {
+            equipmentInGame[27].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 28)
+        {
+            equipmentInGame[28].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 29)
+        {
+            equipmentInGame[29].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 30)
+        {
+            equipmentInGame[30].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 31)
+        {
+            equipmentInGame[31].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 32)
+        {
+            equipmentInGame[32].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 33)
+        {
+            equipmentInGame[33].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 34)
+        {
+            equipmentInGame[34].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 35)
+        {
+            equipmentInGame[35].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 36)
+        {
+            equipmentInGame[36].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 37)
+        {
+            equipmentInGame[37].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 38)
+        {
+            equipmentInGame[38].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 39)
+        {
+            equipmentInGame[39].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 40)
+        {
+            equipmentInGame[40].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 41)
+        {
+            equipmentInGame[41].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 42)
+        {
+            equipmentInGame[42].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 43)
+        {
+            equipmentInGame[43].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 44)
+        {
+            equipmentInGame[44].SetActive(true);
+        }
+
+        if (equipmentpoint.point == 45)
+        {
+            equipmentInGame[45].SetActive(true);
+        }
     }
 
 
